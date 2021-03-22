7384f419e1e9126eee25bda4234ce73e2a6629a9
NightOwl <steelman2019e@gmail.com>
2021-03-22 12:46:18 +0900

    반응형 웹 처리

--- refactoring/js/main.js
+++ refactoring/js/main.js
@@ -1,16 +1,29 @@
 (function (window, document) {
   "use strict";
 
-  const toggleList = document.querySelectorAll(".toggle");
-  const toggleBtn = document.getElementById("toggle-btn");
+  const $toggleList = document.querySelectorAll(".toggle");
+  const $toggleBtn = document.getElementById("toggle-btn");
 
-  toggleBtn.addEventListener("click", () => {
+  $toggleBtn.addEventListener("click", () => {
     toggleElements();
   });
 
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 1024) {
+      offElements();
+      console.log("test");
+    }
+  });
+
   function toggleElements() {
-    toggleList.forEach((toggle) => {
+    $toggleList.forEach((toggle) => {
       toggle.classList.toggle("on");
+    });
+  }
+
+  function offElements() {
+    $toggleList.forEach((toggle) => {
+      toggle.classList.remove("on");
     });
   }
 })(window, document);
